580645eded341a61722cdd8569c3c97140579fbc
Lashaun <clarklas831@gmail.com>
2021-07-05 10:49:40 -0500

    get geolocation from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,28 @@
-let searchLocation = `Milwaukee`;
-let lat;
-let lon;
+let searchLocation = `New York`;
+let latitude = localStorage.getItem(`Latitude`);
+let longitude = localStorage.getItem(`Longitude`);
 
 let geoFinderApi = `http://open.mapquestapi.com/geocoding/v1/address?key=M6cWf6SB2TBYZpZZyd6wL6kpI31d0emQ&location=${searchLocation}`;
-let weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=05259468112d1e5fac09c5030fda1d57`;
+let weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=05259468112d1e5fac09c5030fda1d57`;
 
-function you() {
+function getGeoLocation() {
   fetch(geoFinderApi)
     .then((response) => response.json())
-    .then((data) => console.log(`Geolocation`, data.results[0].locations[0].latLng));
+    .then(function (data) {
+      latitude = data.results[0].locations[0].latLng.lat;
+      longitude = data.results[0].locations[0].latLng.lng;
+      localStorage.setItem(`Longitude`, longitude.toFixed(2));
+      localStorage.setItem(`Latitude`, latitude.toFixed(2));
+      console.log(latitude, longitude);
+    });
 }
 
-function me() {
+function searchWeather() {
   fetch(weatherApiUrl)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then(function (data) {
+      // code here
+    });
 }
 
 // geofinder - results -> locations -> latLng
@@ -22,9 +30,10 @@ function me() {
 // use the geofinder api to find the lat and lon of the user's search
 // use the lat and lon from geofinder to search the location on the openweather api
 
-// function consoley() {
-//   console.log(searchLocation);
-// }
+function readSearch() {
+  searchLocation = $(`input`).val();
+  console.log($(`input`).val());
+}
 
-// $(`.search-button`).on(`click`, consoley);
-// results[0].locations[0].latLng;
+$(`.search-button`).on(`click`, readSearch);
+getGeoLocation();
